1a51c4b270a0decd2a3bd5c99364704f90e4e139
George <george.postoronca@gmail.com>
2020-10-16 09:59:50 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+class Modal {
+  constructor(element) {
+    this.modal = element;
+    if (this.modal) return false;
+  }
+
+  init() {}
+
+  open() {}
+
+  close() {}
+
+  create() {}
+
+  refresh() {}
+}
